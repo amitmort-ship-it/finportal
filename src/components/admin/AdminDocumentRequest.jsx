95d928c12171dc37c5f519cf50b345d042319649
Amit Kushmirak <amit.mort@gmail.com>
2026-03-25 21:01:59 +0000

    File changes

--- src/components/admin/AdminDocumentRequest.jsx
+++ src/components/admin/AdminDocumentRequest.jsx
@@ -50,7 +50,8 @@ export default function AdminDocumentRequest({ selectedClient, onClientChange })
 
   useEffect(() => {
     const load = async () => {
-      const profiles = await base44.entities.ClientProfile.filter({});
+      const res = await base44.functions.invoke('getAllClients', {});
+      const profiles = res.data?.profiles || [];
       setUsers(profiles);
     };
     load();
