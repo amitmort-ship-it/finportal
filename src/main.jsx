39dfc83c355b80e85b7171b26e0866f0f07a879f
amitmort-ship-it <amit.mort@gmail.com>
2026-04-29 22:11:41 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+document.documentElement.lang = 'he'
+document.documentElement.dir = 'rtl'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
